ea475b0a4c793d8c76dff0265015cc222cb1129b
Douglas Yau <douglas.yau97@gmail.com>
2022-08-22 17:24:38 -0600

    Add comments and small adjustments

--- maze.py
+++ maze.py
@@ -1,10 +1,13 @@
-import time
 import turtle
-import sys
 from LHR import Lefthandrule
 from dijkstra import *
 from tkinter import *
 from util import *
+
+"""
+Wall object of shape square and color white
+Forbids the algo from going there
+"""
 
 
 class Wall(turtle.Turtle):
@@ -16,6 +19,12 @@ class Wall(turtle.Turtle):
         self.speed(0)
 
 
+"""
+Path object of shape square and color yellow
+Displays shortest path from start to goal
+"""
+
+
 class Path(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -23,6 +32,12 @@ class Path(turtle.Turtle):
         self.color("yellow")
         self.penup()
         self.speed(0)
+
+
+"""
+Visited object of shape square and color orange
+Shows cells that have been visited
+"""
 
 
 class Visited(turtle.Turtle):
@@ -34,6 +49,11 @@ class Visited(turtle.Turtle):
         self.speed(0)
 
 
+"""
+Goal object of shape square and color green
+"""
+
+
 class Goal(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -41,6 +61,12 @@ class Goal(turtle.Turtle):
         self.color("green")
         self.penup()
         self.speed(0)
+
+
+"""
+User object of shape square and color blue
+User controlled object to traverse the maze
+"""
 
 
 class User(turtle.Turtle):
@@ -76,6 +102,7 @@ class User(turtle.Turtle):
                 print("Player has reached goal")
 
 
+# Initializes and builds maze based on a string file with the indivual letter representing different objects
 def build_maze(maze):
     for y in range(len(maze[0])):
         for x in range(len(maze)):
@@ -84,24 +111,29 @@ def build_maze(maze):
             x_cord = -348 + (x * 24)
             y_cord = 348 - (y * 24)
 
+            # 'x' stands for walls
             if ch == "x":
                 walls.append([x_cord, y_cord])
                 wall.goto(x_cord, y_cord)
                 wall.stamp()
 
+            # 'O' stands for goal
             elif ch == "O":
                 goal.goto(x_cord, y_cord)
                 finish.append([x_cord, y_cord])
                 walls.append([x_cord, y_cord - 24])
                 goal.stamp()
 
+            # 'U' stands for user
             elif ch == "U":
                 user.goto(x_cord, y_cord)
 
+            # 'L' stands for the left hand rule pointer
             elif ch == "L":
                 lhr.goto(x_cord, y_cord)
 
 
+# Runner method for dijkstra algo
 def run_dijkstra(maze):
     visited = Visited()
     solution = Path()
@@ -144,6 +176,7 @@ maze = [
     list("xxxxxxxxxxxxxxxxxxxxxxxxOxxxxx"),
 ]
 
+# initialize classes and vars
 wall = Wall()
 goal = Goal()
 user = User()
@@ -152,13 +185,14 @@ walls = []
 need_lhr = False
 lhr = Lefthandrule(finish, walls)
 
-
+# Initialize window
 win = turtle.Screen()
 win.bgcolor("black")
 win.title("Maze Game")
 win.setup(800, 900)
 canvas = win.getcanvas()
 
+# Buttons to run dijkstra and lhr
 button_dijkstra = Button(canvas.master, text="Dijkstra", font="ariel", command=lambda: run_dijkstra(maze))
 button_dijkstra.place(x=40, y=850)
 button_lhr = Button(canvas.master, text="Left Hand Rule", font="ariel", command=lambda: lhr_solver(lhr, win))
@@ -167,7 +201,7 @@ button_lhr.place(x=120, y=850)
 
 build_maze(maze)
 
-
+# User controls
 turtle.listen()
 turtle.onkey(user.usr_up, "Up")
 turtle.onkey(user.usr_down, "Down")
@@ -177,8 +211,3 @@ turtle.onkey(user.usr_right, "Right")
 
 while True:
     win.update()
-# print(unvisited)
-
-
-print(path)
-# print(deez)
